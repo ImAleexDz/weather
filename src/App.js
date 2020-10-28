ba497b779e6bc1e2fb55a4b6b23108e45808f48c
ImAleexDz <alexndro7145@gmail.com>
2020-10-27 21:57:18 -0600

    add clima component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
+import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
+  //state del formulario 
+  const [busqueda, setBusqueda] = useState({
+    ciudad: '',
+    pais: ''
+  });
+
+  const [consultar, setConsultar] = useState(false);
+  const [resultado, setResultado] = useState({});
+  const [error, setError] = useState(false);
+
+  const { ciudad, pais } = busqueda;
+
+  //UseEffect para detectar los cambios
+  useEffect(() => {
+    const consultarApi = async () => {
+      if(consultar) {
+        const appid = '8cdc7103aae645083664b78c69b65ec0';
+        const URL = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appid}`
+
+        const respuesta = await fetch(URL);
+        const resultado = await respuesta.json()
+
+        setResultado(resultado);
+        setConsultar(false);
+
+        //Detecta si hubo resultados concretos en la consulta
+        if(resultado.cod === "400") {
+          setError(true)
+        } else {
+          setError(false)
+        }
+      }
+      
+    }
+    consultarApi();
+  }, [consultar]);
+
+  //Carga condicional de componentes
+
+  let componente;
+  if(error) {
+    componente = <Error mensaje="No hay resultados"/>
+  } else {
+    componente = <Clima resultado={resultado}/>
+  }
+
   return (
     <Fragment>
       <Header 
@@ -12,10 +60,14 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col m6 s12">
-              <Form />
+              <Form 
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsultar={setConsultar}
+              />
             </div>
             <div className="col m6 s12">
-              2
+              {componente}
             </div>
           </div>
         </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Error from './Error'
 
-const Form = () => {
+const Form = ({busqueda, setBusqueda, setConsultar}) => {
+
+    const [error, setError] = useState(false);
+
+    const { ciudad, pais } = busqueda;
+
+    //funcion que coloca los elementos en el state
+    const handleChange = e => {
+        //actualizar el state
+        setBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //Cuando el usuario da submit al form
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        //Validar
+        if(ciudad.trim() === '' || pais.trim() === '') {
+            setError(true);
+            return;
+        }
+
+        setError(false);
+        setConsultar(true);
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
+            {error ? <Error mensaje="Todos los campos son obligatorios"/> : null}
             <div className="input-field col s12">
                 <input 
                     type="text"
                     name="ciudad"
                     id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
                 />
                 <label htmlFor="ciudad">Ciudad: </label>
             </div>
@@ -16,6 +50,8 @@ const Form = () => {
                 <select
                     name="pais"
                     id="pais"
+                    value={pais}
+                    onChange={handleChange}
                 >
                     <option value=""> Seleccione un país </option>
                     <option value="US">Estados Unidos</option>
@@ -27,6 +63,16 @@ const Form = () => {
                     <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="pais">País: </label>
+            </div>
+
+            <div className="input-field col s12">
+                <button
+                    type="submit"
+                    value="Buscar Clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                >
+                    Buscar Clima
+                </button>
             </div>
         </form>
     )
